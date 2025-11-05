a051db4218db8cf6bb861a9fef0d5c947d85178f
Dhatshayani Natarajan <dhatshayaninatarajan@Dhatshayanis-MacBook-Pro-3.local>
2025-11-05 12:59:09 +0530

    added terraform deployment config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,65 @@
-module "dpg_novmeber_elb" {
-  source                   = "./modules/elastic_load_balancer"
-  security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet.sn.id
-  UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
 }
 
-module "dpg_november_asg" {
-  source                   = "./modules/autoscaling_group"
-  security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet.sn.id
-  elb_id                   = module.dpg_novmeber_elb.elb_id
-  UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
-  ARTIFACT                 = var.ARTIFACT
+provider "aws" {
+  region = "us-east-1"
 }
-data "aws_security_group" "sg" {
-  tags = {
-    "Purpose" = "Playground"
+
+resource "aws_s3_bucket" "react_app_bucket" {
+  bucket = "devops-deploy-project"   # bucket name you gave
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
   }
 }
-data "aws_subnet" "sn"{
-  vpc_id = data.aws_vpc.vpc.id
-  tags = {
-    Purpose = "Playground"
-    count = 0
-  }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.react_app_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
-data "aws_vpc" "vpc"{
-  tags = {
-    Purpose = "Playground"
-  }
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.react_app_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.react_app_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_object" "react_build_files" {
+  for_each = fileset("../build", "**/*.*")
+
+  bucket = aws_s3_bucket.react_app_bucket.id
+  key    = each.value
+  source = "../build/${each.value}"
+  acl    = "public-read"
+
+  content_type = lookup({
+    html = "text/html",
+    css  = "text/css",
+    js   = "application/javascript",
+    json = "application/json",
+    png  = "image/png",
+    jpg  = "image/jpeg",
+    svg  = "image/svg+xml"
+  }, split(".", each.value)[1], "application/octet-stream")
 }
